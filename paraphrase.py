2fc70a3e62d0830c1721e69f76a3af86e5be52cf
root <aszymczy>
2023-01-12 17:43:27 +0100

    Added option to choose which words to switch, cleaned spaghetti code in functions, updated constructor

--- paraphrase.py
+++ paraphrase.py
@@ -1,13 +1,19 @@
 import random
 from lyrics import get_text
+from termcolor import colored
 
 
 class Paraphrase:
-    def __init__(self, lyrics):
+    def __init__(self, url, lyrics, option="synonym",
+                 change="first", variety=5):
+        self.url = url
         self._lyrics = lyrics.lines()
+        self.option = option
+        self.change = change
+        self.variety = variety
 
-    def count_syllabs(self, data):
-        return data["numSyllables"]
+    def highlight_text(self, word):
+        return colored(word, "green")
 
     def correct_upper(self, word, new_word):
         if word.isupper():
@@ -20,46 +26,67 @@ class Paraphrase:
         marks = """.,!?:;'-")"""
         if word[-1] in marks:
             new_word += word[-1]
-        if word[0] in "(\"":
+        if word[0] in '("':
             new_word = word[0] + new_word
         return new_word
 
-    def choose_words(self, word, option, variety):
+    def choose_words(self, word):
         results = []
-        data = get_text(option, word.strip(".,\" !?';:()-"), variety)
+        data = get_text(self.url, self.option,
+                        word.strip(".,\" !?';:()-"), self.variety)
         for words in data:
             if len(word) >= 4:
                 results.append(words["word"])
         return results
 
-    def switch_words(self, line, option, variety):
+    def switch_words(self, line):
         new_line = []
-        for word in line.split():
-            data = self.choose_words(word, option, variety)
-            if data:
-                new_word = random.choice(data)
-                new_word = self.correct_upper(word, new_word)
-                new_line.append(self.correct_punctuation(word, new_word))
-            else:
-                new_line.append(word)
+        if self.change == "all":
+            for word in line.split():
+                new_line.append(self.randomize_word(word))
+        elif self.change == "first":
+            new_line = line.split()
+            if new_line:
+                new_line[0] = self.randomize_word(new_line[0])
+        elif self.change == "last":
+            new_line = line.split()
+            if new_line:
+                new_line[-1] = self.randomize_word(new_line[-1])
+        else:
+            new_line = line.split()
+            picked = random.choice(new_line)
+            index = new_line.index(picked)
+            new_line[index] = self.randomize_word(new_line[index])
         return " ".join(new_line)
 
-    def create_lyrics(self, option, variety=5):
+    def randomize_word(self, word):
+        data = self.choose_words(word)
+        if data:
+            new_word = random.choice(data)
+            new_word = self.correct_upper(word, new_word)
+            new_word = self.highlight_text(new_word)
+            new_word = self.correct_punctuation(word, new_word)
+            return new_word
+        else:
+            return word
+
+    def create_lyrics(self):
         new_lyrics = []
         for line in self._lyrics:
-            if option == "adjective":
-                new_lyrics.append(self.add_adjectives(line, variety))
+            if self.option == "adjective":
+                new_lyrics.append(self.add_adjectives(line))
             else:
-                new_lyrics.append(self.switch_words(line, option, variety))
+                new_lyrics.append(self.switch_words(line))
         return "\n".join(new_lyrics)
 
-    def add_adjectives(self, line, variety):
+    def add_adjectives(self, line):
         new_line = []
         for word in line.split():
-            data = self.choose_words(word, "adjective", variety)
+            data = self.choose_words(word)
             if data:
                 adjective = random.choice(data)
                 adjective = self.correct_upper(word, adjective)
+                adjective = self.highlight_text(adjective)
                 new_line.append(adjective)
                 new_line.append(word)
             else:
