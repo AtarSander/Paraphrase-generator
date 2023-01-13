5ed65b71a71c3cf29818b7d902908f9d5168c9b3
root <aszymczy>
2023-01-13 17:03:51 +0100

    Reworked adding adjectives and creating lyrics, added all necessary tests

--- paraphrase.py
+++ paraphrase.py
@@ -12,17 +12,20 @@ class Paraphrase:
         self.change = change
         self.variety = variety
 
-    def highlight_text(self, word):
+    @staticmethod
+    def highlight_text(word):
         return colored(word, "green")
 
-    def correct_upper(self, word, new_word):
+    @staticmethod
+    def correct_upper(word, new_word):
         if word.isupper():
             return new_word.upper()
         if word[0].isupper() or word[1].isupper():
             return new_word.capitalize()
         return new_word
 
-    def correct_punctuation(self, word, new_word):
+    @staticmethod
+    def correct_punctuation(word, new_word):
         marks = """.,!?:;'-")"""
         if word[-1] in marks:
             new_word += word[-1]
@@ -59,36 +62,72 @@ class Paraphrase:
             new_line[index] = self.randomize_word(new_line[index])
         return " ".join(new_line)
 
-    def randomize_word(self, word):
+    def randomize_word(self, word, adjective=False):
         data = self.choose_words(word)
         if data:
             new_word = random.choice(data)
-            new_word = self.correct_upper(word, new_word)
-            new_word = self.highlight_text(new_word)
-            new_word = self.correct_punctuation(word, new_word)
+            if not adjective:
+                new_word = self.correct_upper(word, new_word)
+                new_word = self.highlight_text(new_word)
+                new_word = self.correct_punctuation(word, new_word)
             return new_word
+        else:
+            if not adjective:
+                return word
+            else:
+                return None
+
+    def insert_adjective(self, line, index, all=False):
+        word = line[index]
+        new_word = self.randomize_word(word, True)
+        if new_word:
+            if index == 0:
+                new_word = self.correct_upper("Capital letter", new_word)
+                word = word.lower()
+                line[0] = word
+            new_word = self.highlight_text(new_word)
+            if all:
+                return new_word, word
+            line.insert(index, new_word)
+        if not all:
+            return line
         else:
             return word
 
+    def add_adjectives(self, line):
+        words = line.split()
+        new_line = words
+        if self.change == "all":
+            new_line = []
+            for index, _ in enumerate(words):
+                adjective_tuple = self.insert_adjective(words, index, True)
+                if type(adjective_tuple) == tuple:
+                    new_line.append(adjective_tuple[0])
+                    new_line.append(adjective_tuple[1])
+                else:
+                    new_line.append(adjective_tuple)
+        elif self.change == "first":
+            new_line = self.insert_adjective(new_line, 0)
+        elif self.change == "last":
+            new_line = self.insert_adjective(new_line, -1)
+        else:
+            picked = random.choice(words)
+            index = new_line.index(picked)
+            new_line = self.insert_adjective(new_line, index)
+        return " ".join(new_line)
+
     def create_lyrics(self):
         new_lyrics = []
-        for line in self._lyrics:
-            if self.option == "adjective":
-                new_lyrics.append(self.add_adjectives(line))
-            else:
-                new_lyrics.append(self.switch_words(line))
+        if self.option == "adjective":
+            for line in self._lyrics:
+                if line:
+                    new_lyrics.append(self.add_adjectives(line))
+                else:
+                    new_lyrics.append(line)
+        else:
+            for line in self._lyrics:
+                if line:
+                    new_lyrics.append(self.switch_words(line))
+                else:
+                    new_lyrics.append(line)
         return "\n".join(new_lyrics)
-
-    def add_adjectives(self, line):
-        new_line = []
-        for word in line.split():
-            data = self.choose_words(word)
-            if data:
-                adjective = random.choice(data)
-                adjective = self.correct_upper(word, adjective)
-                adjective = self.highlight_text(adjective)
-                new_line.append(adjective)
-                new_line.append(word)
-            else:
-                new_line.append(word)
-        return " ".join(new_line)
--- test_paraphrase.py
+++ test_paraphrase.py
@@ -1,163 +1,341 @@
 from paraphrase import Paraphrase
-from lyrics import Lyrics
+from lyrics import Lyrics, setup_config
 import random
 
-TEXT2 = [
-  {
-    "title": "Prayer for Children",
-    "author": "William Cowper",
-    "lines": [
-      "Gracious Lord, our children see,",
-      "By Thy mercy we are free;",
-      "But shall these, alas! remain",
-      "Subjects still of Satan's reign?",
-      "Israel's young ones, when of old",
-      "Pharaoh threaten'd to withhold,",
-      "Then Thy messenger said, \"No;",
-      "Let the children also go!\"",
-      "",
-      "When the angel of the Lord,",
-      "Drawing forth his dreadful sword,",
-      "Slew with an avenging hand,",
-      "All the first-born of the land;",
-      "Then Thy people's door he pass'd,",
-      "Where the bloody sign was placed:",
-      "Hear us, now, upon our knees,",
-      "Plead the blood of Christ for these!"
-    ],
-    "linecount": "16"
-  }
-]
 
-TEXT3 = [
-  {
-    "title": "Cancelled Passage of Mont Blanc",
-    "author": "Percy Bysshe Shelley",
-    "lines": [
-      "There is a voice, not understood by all,",
-      "Sent from these desert-caves. It is the roar",
-      "Of the rent ice-cliff which the sunbeams call,",
-      "Plunging into the vale--it is the blast",
-      "Descending on the pines--the torrents pour..."
-    ],
-    "linecount": "5"
-  }
-]
-
-
-TEXT4 = [
-  {
-    "title": "His Heart was darker than the starless night",
-    "author": "Emily Dickinson",
-    "lines": [
-      "His Heart was darker than the starless night",
-      "For that there is a morn",
-      "But in this black Receptacle",
-      "Can be no Bode of Dawn"
-    ],
-    "linecount": "4"
-  }
-]
+test_texts = setup_config("config.json", "test_text")
+CONFIG = setup_config("config.json", "urls")
+TEXT1 = test_texts["text1_medium"]
+TEXT2 = test_texts["text2_short"]
+TEXT3 = test_texts["text3_short"]
+TEXT4 = test_texts["text4_medium"]
+TEXT5 = test_texts["text5_short"]
 
 
 def test_Paraphrase_correct_upper_capitalize():
-    example_poem = Lyrics(TEXT2)
-    generator = Paraphrase(example_poem)
+    example_poem = Lyrics(TEXT1)
+    generator = Paraphrase(CONFIG, example_poem)
     assert generator.correct_upper("Gracious", "good") == "Good"
 
 
 def test_Paraphrase_correct_upper_all():
-    example_poem = Lyrics(TEXT2)
-    generator = Paraphrase(example_poem)
+    example_poem = Lyrics(TEXT1)
+    generator = Paraphrase(CONFIG, example_poem)
     assert generator.correct_upper("GRACIOUS", "good") == "GOOD"
 
 
 def test_Paraphrase_correct_punctuation_end():
-    example_poem = Lyrics(TEXT2)
-    generator = Paraphrase(example_poem)
+    example_poem = Lyrics(TEXT1)
+    generator = Paraphrase(CONFIG, example_poem)
     assert generator.correct_punctuation("sword,", "blade") == "blade,"
 
 
 def test_Paraphrase_correct_punctuation_start():
-    example_poem = Lyrics(TEXT2)
-    generator = Paraphrase(example_poem)
+    example_poem = Lyrics(TEXT1)
+    generator = Paraphrase(CONFIG, example_poem)
     assert generator.correct_punctuation("(sword", "blade") == "(blade"
 
 
 def test_Paraphrase_correct_punctuation_both():
-    example_poem = Lyrics(TEXT2)
-    generator = Paraphrase(example_poem)
+    example_poem = Lyrics(TEXT1)
+    generator = Paraphrase(CONFIG, example_poem)
     assert generator.correct_punctuation("(sword,", "blade") == "(blade,"
 
 
 def test_Paraphrase_choose_words_synonyms():
-    example_poem = Lyrics(TEXT2)
-    generator = Paraphrase(example_poem)
-    assert generator.choose_words("gracious", "synonym", 6) == ["good",
-                                                                "kind",
-                                                                "nice",
-                                                                "benevolent",
-                                                                "elegant",
-                                                                "propitious"]
+    example_poem = Lyrics(TEXT1)
+    generator = Paraphrase(CONFIG, example_poem, "synonym", "all", 6)
+    assert generator.choose_words("gracious") == ["good",
+                                                  "kind",
+                                                  "nice",
+                                                  "benevolent",
+                                                  "elegant",
+                                                  "propitious"]
 
 
 def test_Paraphrase_choose_words_rhymes():
-    example_poem = Lyrics(TEXT3)
-    generator = Paraphrase(example_poem)
-    assert generator.choose_words("pour", "rhyme", 4) == ["for",
-                                                          "pore",
-                                                          "tor",
-                                                          "outpour"]
-
-
-def test_Paraphrase_switch_words_synonyms(monkeypatch):
     example_poem = Lyrics(TEXT2)
-    generator = Paraphrase(example_poem)
+    generator = Paraphrase(CONFIG, example_poem, "rhyme", "all", 4)
+    assert generator.choose_words("pour") == ["for",
+                                              "pore",
+                                              "tor",
+                                              "outpour"]
+
+
+def test_Paraphrase_choose_words_adjectives():
+    pass
+
+
+def test_Paraphrase_switch_words_synonyms_all(monkeypatch):
+    example_poem = Lyrics(TEXT1)
+    generator = Paraphrase(CONFIG, example_poem, "synonym", "all", 5)
 
     def mock_choice(data):
         return data[0]
     monkeypatch.setattr(random, "choice", mock_choice)
     line = example_poem.lines()[9]
     correct_line = "Once the saint of the Master,"
-    assert generator.switch_words(line, "synonym", 5) == correct_line
+    assert generator.switch_words(line) == correct_line
 
 
-def test_Paraphrase_switch_words_rhymes(monkeypatch):
-    example_poem = Lyrics(TEXT2)
-    generator = Paraphrase(example_poem)
+def test_Paraphrase_switch_words_synonyms_last(monkeypatch):
+    example_poem = Lyrics(TEXT1)
+    generator = Paraphrase(CONFIG, example_poem, "synonym", "last", 5)
+
+    def mock_choice(data):
+        return data[0]
+    monkeypatch.setattr(random, "choice", mock_choice)
+    line = example_poem.lines()[9]
+    correct_line = "When the angel of the Master,"
+    assert generator.switch_words(line) == correct_line
+
+
+def test_Paraphrase_switch_words_synonyms_first(monkeypatch):
+    example_poem = Lyrics(TEXT1)
+    generator = Paraphrase(CONFIG, example_poem, "synonym", "first", 5)
+
+    def mock_choice(data):
+        return data[0]
+    monkeypatch.setattr(random, "choice", mock_choice)
+    line = example_poem.lines()[9]
+    correct_line = "Once the angel of the Lord,"
+    assert generator.switch_words(line) == correct_line
+
+
+def test_Paraphrase_switch_words_synonyms_random(monkeypatch):
+    example_poem = Lyrics(TEXT1)
+    generator = Paraphrase(CONFIG, example_poem, "synonym", "random", 5)
+
+    def mock_choice(data):
+        return data[3]
+    monkeypatch.setattr(random, "choice", mock_choice)
+
+    line = example_poem.lines()[10]
+    correct_line = "Drawing forth his awful sword,"
+    assert generator.switch_words(line) == correct_line
+
+
+def test_Paraphrase_switch_words_rhymes_all(monkeypatch):
+    example_poem = Lyrics(TEXT1)
+    generator = Paraphrase(CONFIG, example_poem, "rhyme", "all")
 
     def mock_choice(data):
         return data[0]
     monkeypatch.setattr(random, "choice", mock_choice)
     line = example_poem.lines()[0]
     correct_line = "Loquacious Overlord, our grandchildren foresee,"
-    assert generator.switch_words(line, "rhyme", 5) == correct_line
+    assert generator.switch_words(line) == correct_line
 
 
-def test_Paraphrase_create_lyrics_synonyms(monkeypatch):
-    example_poem = Lyrics(TEXT3)
-    generator = Paraphrase(example_poem)
+def test_Paraphrase_switch_words_rhymes_last(monkeypatch):
+    example_poem = Lyrics(TEXT1)
+    generator = Paraphrase(CONFIG, example_poem, "rhyme", "last")
 
     def mock_choice(data):
         return data[0]
     monkeypatch.setattr(random, "choice", mock_choice)
+    line = example_poem.lines()[0]
+    correct_line = "Gracious Lord, our children foresee,"
+    assert generator.switch_words(line) == correct_line
+
+
+def test_Paraphrase_switch_words_rhymes_first(monkeypatch):
+    example_poem = Lyrics(TEXT1)
+    generator = Paraphrase(CONFIG, example_poem, "rhyme", "first")
+
+    def mock_choice(data):
+        return data[0]
+    monkeypatch.setattr(random, "choice", mock_choice)
+    line = example_poem.lines()[10]
+    correct_line = "Withdrawing forth his dreadful sword,"
+    assert generator.switch_words(line) == correct_line
+
+
+def test_Paraphrase_switch_words_rhymes_random(monkeypatch):
+    example_poem = Lyrics(TEXT3)
+    generator = Paraphrase(CONFIG, example_poem, "rhyme", "random", 5)
+
+    def mock_choice(data):
+        return data[1]
+    monkeypatch.setattr(random, "choice", mock_choice)
+    line = example_poem.lines()[0]
+    correct_line = "His Tart was darker than the starless night"
+    assert generator.switch_words(line) == correct_line
+
+
+def test_Paraphrase_insert_adjective(monkeypatch):
+    example_poem = Lyrics(TEXT1)
+    generator = Paraphrase(CONFIG, example_poem, "adjective", "first", 1)
+    line = example_poem.lines()[0]
+    line = line.split()
+    correct_line = "Good gracious Lord, our children see,"
+    assert " ".join(generator.insert_adjective(line, 0)) == correct_line
+
+
+def test_Paraphrase_create_lyrics_synonyms_all():
+    example_poem = Lyrics(TEXT2)
+    generator = Paraphrase(CONFIG, example_poem, "synonym", "all", 1)
     correct_lyrics = """Thither is a sound, not tacit by complete,
 Conveyed from these desert-caves. It is the boom
 Of the take ice-cliff which the sunbeams bid,
 Plunging into the vale--it is the fire
 Descending on the pines--the torrents stream."""
-    assert generator.create_lyrics("synonym") == correct_lyrics
+    assert generator.create_lyrics() == correct_lyrics
 
 
-def test_Paraphrase_create_lyrics_rhymes(monkeypatch):
+def test_Paraphrase_create_lyrics_synonyms_first():
     example_poem = Lyrics(TEXT4)
-    generator = Paraphrase(example_poem)
+    generator = Paraphrase(CONFIG, example_poem, "synonym", "first", 1)
+    correct_lyrics = """World affects to be sedate
+Upon occasion, grand
+But let our observation shut
+Her practices extend
 
-    def mock_choice(data):
-        return data[0]
+To Necromancy and the Trades
+Far to understand
+Lay eyes on our spacious Citizen
+Unto a Juggler turned --"""
+    assert generator.create_lyrics() == correct_lyrics
+
+
+def test_Paraphrase_create_lyrics_synonyms_last():
+    example_poem = Lyrics(TEXT2)
+    generator = Paraphrase(CONFIG, example_poem, "synonym", "last", 1)
+    correct_lyrics = """There is a voice, not understood by complete,
+Sent from these desert-caves. It is the boom
+Of the rent ice-cliff which the sunbeams bid,
+Plunging into the vale--it is the fire
+Descending on the pines--the torrents stream."""
+    assert generator.create_lyrics() == correct_lyrics
+
+
+def test_Paraphrase_create_lyrics_synonyms_random(monkeypatch):
+    example_poem = Lyrics(TEXT5)
+    generator = Paraphrase(CONFIG, example_poem, "synonym", "random")
+
+    def mock_choice(words):
+        if not words:
+            return ""
+        elif len(words) > 1:
+            return words[1]
+        else:
+            return words[0]
     monkeypatch.setattr(random, "choice", mock_choice)
+    correct_lyrics = """The Mount sat upon the Plain
+In his tremendous Chair --
+His reflection omnifold,
+His inquest, everywhere --\n
+The Seasons played around his knees
+Like Children round a sire --
+Grandfather of the Days is He
+Of Penetrate, the Ancestor --"""
+    assert generator.create_lyrics() == correct_lyrics
+
+
+def test_Paraphrase_create_lyrics_rhymes_all():
+    example_poem = Lyrics(TEXT3)
+    generator = Paraphrase(CONFIG, example_poem, "rhyme", "all", 1)
     correct_lyrics = """His Part was darker none the starless knight
 For but where is a morne
 But in diss blackjack Receptacle
 Can be no Abode of Don"""
-    assert generator.create_lyrics("rhyme") == correct_lyrics
+    assert generator.create_lyrics() == correct_lyrics
+
+
+def test_Paraphrase_create_lyrics_rhymes_last():
+    example_poem = Lyrics(TEXT2)
+    generator = Paraphrase(CONFIG, example_poem, "rhyme", "last", 1)
+    correct_lyrics = """There is a voice, not understood by overall,
+Sent from these desert-caves. It is the fore
+Of the rent ice-cliff which the sunbeams recall,
+Plunging into the vale--it is the past
+Descending on the pines--the torrents for."""
+    assert generator.create_lyrics() == correct_lyrics
+
+
+def test_Paraphrase_create_lyrics_rhymes_first():
+    example_poem = Lyrics(TEXT2)
+    generator = Paraphrase(CONFIG, example_poem, "rhyme", "first", 1)
+    correct_lyrics = """Where is a voice, not understood by all,
+Went from these desert-caves. It is the roar
+Of the rent ice-cliff which the sunbeams call,
+Plunging into the vale--it is the blast
+Ascending on the pines--the torrents pour..."""
+    assert generator.create_lyrics() == correct_lyrics
+
+
+def test_Paraphrase_create_lyrics_rhymes_random(monkeypatch):
+    example_poem = Lyrics(TEXT5)
+    generator = Paraphrase(CONFIG, example_poem, "rhyme", "random")
+
+    def mock_choice(words):
+        if not words:
+            return ""
+        elif len(words) > 1:
+            return words[1]
+        else:
+            return words[0]
+    monkeypatch.setattr(random, "choice", mock_choice)
+    correct_lyrics = """The Mountain sat upon the Plain
+In his tremendous Chair --
+His consideration omnifold,
+His quest, everywhere --\n
+The Seasons played around his knees
+Like Schoolchildren round a sire --
+Grandfather of the Days is He
+Of Predawn, the Ancestor --"""
+    assert generator.create_lyrics() == correct_lyrics
+
+
+def test_Paraphrase_create_lyrics_adjectives_all():
+    example_poem = Lyrics(TEXT2)
+    generator = Paraphrase(CONFIG, example_poem, "adjective", "all", 1)
+    correct_lyrics = """Getting there is a low voice, not little understood by cell all,
+Haughty sent letter from try these desert-caves. It is the deafening roar
+Of the annual rent ice-cliff new which the golden sunbeams first call,
+Deep plunging dissolve into the vale--it is the full blast
+Own descending on the pines--the such torrents pece pour..."""
+    assert generator.create_lyrics() == correct_lyrics
+
+
+def test_Paraphrase_create_lyrics_adjectives_last():
+    example_poem = Lyrics(TEXT2)
+    generator = Paraphrase(CONFIG, example_poem, "adjective", "last", 1)
+    correct_lyrics = """There is a voice, not understood by cell all,
+Sent from these desert-caves. It is the deafening roar
+Of the rent ice-cliff which the sunbeams first call,
+Plunging into the vale--it is the full blast
+Descending on the pines--the torrents pece pour..."""
+    assert generator.create_lyrics() == correct_lyrics
+
+
+def test_Paraphrase_create_lyrics_adjectives_first():
+    example_poem = Lyrics(TEXT4)
+    generator = Paraphrase(CONFIG, example_poem, "adjective", "first", 1)
+    correct_lyrics = """Human nature affects to be sedate
+Agreed upon occasion, grand
+But let our observation shut
+Her practices extend\n
+To Necromancy and the Trades
+Immense remote to understand
+Young behold our spacious Citizen
+Same unto a Juggler turned --"""
+    assert generator.create_lyrics() == correct_lyrics
+
+
+def test_Paraphrase_create_lyrics_adjectives_random(monkeypatch):
+    example_poem = Lyrics(TEXT4)
+    generator = Paraphrase(CONFIG, example_poem, "adjective", "random")
+
+    def mock_choice(data):
+        return data[1]
+    monkeypatch.setattr(random, "choice", mock_choice)
+    correct_lyrics = """Nature positive affects to be sedate
+Upon present occasion, grand
+But let our observation shut
+Her best practices extend\n
+To such Necromancy and the Trades
+Remote to understand
+Behold our spacious Citizen
+Unto a Juggler turned --"""
+    assert generator.create_lyrics() == correct_lyrics
